f37fa1d23654a1eae99ae2d43e9767f1808e054a
alecglassford <glassford@stanford.edu>
2017-05-13 16:22:06 -0700

    Add sample incoming call

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from twilio.twiml.voice_response import VoiceResponse
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Hello world'
+
+@app.route('/incoming-call')
+def incoming_call():
+    resp = VoiceResponse()
+    resp.play('http://www.flatterist.com/woiu.mp3')
+    return str(resp)
 
 if __name__ == '__main__':
     app.run()
